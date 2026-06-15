d7d51a5649877f98df13a3bb945eedb345765ced
ayushsid9 <ayushsid9.com>
2026-06-16 01:21:51 +0530

    Configure production deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,3 +4,9 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+export default API;
